cced0c416e81f190c7786eb2be7051325e9f0a42
gyaan <gyaan1334@gmail.com>
2019-05-06 23:44:23 +0530

    packaging

--- cmd/app1/app1.go
+++ cmd/app1/app1.go
@@ -1,4 +1,4 @@
-package main
+package app1
 
 import "fmt"
 
--- hello.go
+++ hello.go
@@ -1,11 +1,17 @@
-package hello
+package main
 
 import (
     "fmt"
     "rsc.io/quote"
+    app1 "github.com/gyaan/go-module-test/cmd/app1"
+    app2 "github.com/gyaan/go-module-test/cmd/app2"
 )
 
+func main() {
+    testHello()
+}
 func testHello() {
     fmt.Println(quote.Hello())
-    fmt.Println()
+    app2.TestApp2()
+    app1.TestApp1()
 }
